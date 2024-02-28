d64af38c3b6e840c3229b0800c5ea8d50d15b232
WeberJoachim <joachimweber@posteo.de>
2024-02-28 12:33:52 +0100

    update: serialise for performance

--- shortread_RNAseq/extract_motif_information.R
+++ shortread_RNAseq/extract_motif_information.R
@@ -1,4 +1,21 @@
-	library("tidyverse")
+library("tidyverse")
+
+
+process_motif_data <- function(data, motif_column, filter_motifs) {
+  motif = unlist(strsplit(motif_column, ".", fixed = T))[1]
+  data %>%
+    separate_rows(motif_column, sep = '\\),' ) %>%
+    separate(., motif_column, into = c(paste0("Number_Motif_", motif), paste0("Strand_", motif), paste0("Value_", motif_column)), sep = ",") %>%
+    separate(., paste0("Number_Motif_", motif), into = c(paste0("Distance_", motif), paste0("Motif_", motif)), sep = "\\(") %>%
+    filter(., get(paste0("Strand_", motif)) == Strand & !motif %in% filter_motifs)
+}
+
+
+
+
+
+
+
 	args <- commandArgs(trailingOnly = TRUE)
 
 	if (length(args) != 1) {
@@ -10,42 +27,22 @@
 	name = substr(input_file, start = 1, stop = nchar(input_file)-4)
 	print(name)
         data = read.delim(input_file, header = T)
-        data = separate_rows(data, "AAUAAA.Distance.From.Peak.sequence.strand.conservation.", sep = '\\),' )
-        data = separate(data, "AAUAAA.Distance.From.Peak.sequence.strand.conservation.", into = c("A_Number_Motif", "A_Strand", "A_Value"), sep = ",")
-        data = separate(data, "A_Number_Motif", into = c("A_Distance", "A_Motif"), sep = "\\(")
-        data = data %>% filter(Strand == A_Strand & A_Motif != "TTTTTT" & A_Motif != "AAAAAA")
+       
 
-	print("1")
-	print(head(data))
+        data = process_motif_data(data, "AAUAAA.Distance.From.Peak.sequence.strand.conservation.", c("AAAAAA", "TTTTTT") )
+        data = process_motif_data(data, "UUGUUU.Distance.From.Peak.sequence.strand.conservation.", c("TTTTTT", "AAAAAA") )
+        data = process_motif_data(data, "TGTA.Distance.From.Peak.sequence.strand.conservation.", c(""))
+        data = process_motif_data(data, "YA.Distance.From.Peak.sequence.strand.conservation.", c(""))
 
-        data = separate_rows(data, "UUGUUU.Distance.From.Peak.sequence.strand.conservation.", sep = '\\),' )
-        data = separate(data, "UUGUUU.Distance.From.Peak.sequence.strand.conservation.", into = c("U_Number_Motif", "U_Strand", "U_Value"), sep = ",")
-        data = separate(data, "U_Number_Motif", into = c("U_Distance", "U_Motif"), sep = "\\(")
-        data = data %>% filter(Strand == U_Strand & U_Motif != "TTTTTT" & U_Motif != "AAAAAA")
-
-	print("2")
-
-        data = separate_rows(data, "TGTA.Distance.From.Peak.sequence.strand.conservation.", sep = '\\),' )
-        data = separate(data, "TGTA.Distance.From.Peak.sequence.strand.conservation.", into = c("USE_Number_Motif", "USE_Strand", "USE_Value"), sep = ",")
-        data = separate(data, "USE_Number_Motif", into = c("USE_Distance", "USE_Motif"), sep = "\\(")
-        data = data %>% filter(Strand == USE_Strand)
-
-	print("3")
-
-        data = separate_rows(data, "YA.Distance.From.Peak.sequence.strand.conservation.", sep = '\\),' )
-        data = separate(data, "YA.Distance.From.Peak.sequence.strand.conservation.", into = c("YA_Number_Motif", "YA_Strand", "YA_Value"), sep = ",")
-        data = separate(data, "YA_Number_Motif", into = c("YA_Distance", "YA_Motif"), sep = "\\(")
-        data = data %>% filter(Strand == YA_Strand)
-
-        data$A_Distance = as.numeric(data$A_Distance)
-        data$U_Distance = as.numeric(data$U_Distance)
-        data$USE_Distance = as.numeric(data$USE_Distance)
+        data$AAUAAA_Distance = as.numeric(data$AAUAAA_Distance)
+        data$UUGUUU_Distance = as.numeric(data$UUGUUU_Distance)
+        data$TGTA_Distance = as.numeric(data$TGTA_Distance)
         data$YA_Distance = as.numeric(data$YA_Distance)
 
 	print("4")
 
-        positive = data %>% filter(Strand == "+" & 131 > (A_Distance - USE_Distance) & (A_Distance - USE_Distance) > 59 & 28 > (U_Distance - A_Distance) & (U_Distance - A_Distance) > 5 & YA_Distance - A_Distance > 0 )
-        negative = data %>% filter(Strand == "-" & 131 > (USE_Distance - A_Distance) & (USE_Distance - A_Distance) > 59 & 20 > (U_Distance - A_Distance) & (U_Distance - A_Distance) > 5 & A_Distance - YA_Distance > 0 )
+        positive = data %>% filter(Strand == "+" & 131 > (AAUAAA_Distance - TGTA_Distance) & (AAUAAA_Distance - TGTA_Distance) > 59 & 28 > (UUGUUU_Distance - AAUAAA_Distance) & (UUGUUU_Distance - AAUAAA_Distance) > 5 & YA_Distance - AAUAAA_Distance > 0 )
+        negative = data %>% filter(Strand == "-" & 131 > (TGTA_Distance - AAUAAA_Distance) & (TGTA_Distance - AAUAAA_Distance) > 59 & 20 > (UUGUUU_Distance - AAUAAA_Distance) & (UUGUUU_Distance - AAUAAA_Distance) > 5 & AAUAAA_Distance - YA_Distance > 0 )
 
 
         perfekte = rbind(positive, negative)
